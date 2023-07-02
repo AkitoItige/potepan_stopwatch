db7b37533d360d7b5b8f15f82153bf19e7d42a65
AkitoItige <jev_akito@aol.com>
2023-07-02 21:45:36 +0900

    second commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const stopWatch = document.getElementById('stopwatch');
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+let timeLag = 0;
+let intervalId = null;
+
+stop.disabled = true;
+reset.disabled = true;
+
+const updateTime = () => {
+    const miliSecond = timeLag % 1000;
+    const second = Math.floor(timeLag / 1000) % 60;
+    const minute = Math.floor(timeLag / (1000 * 60)) % 60;
+    const hour = Math.floor(timeLag / (1000 * 60 * 60));
+
+    const miliSecondValue = miliSecond.toString().padStart(1, '0');
+    const secondValue = second.toString().padStart(1, '0');
+    const minuteValue = minute.toString().padStart(1, '0');
+    const hourValue = hour.toString().padStart(1, '0');
+
+    stopWatch.innerHTML = `${hourValue}:${minuteValue}:${secondValue}:${miliSecondValue}`;
+}
+
+start.addEventListener('click', function (e) {
+    start.disabled = true;
+    stop.disabled = false;
+    reset.disabled = false;
+    if (intervalId !== null) { return; }
+    let pre = new Date();
+    intervalId = setInterval(function () {
+        const now = new Date();
+        timeLag += now - pre;
+        pre = now;
+        updateTime();
+    }, 10);
+})
+
+stop.addEventListener('click', function (e) {
+    start.disabled = false;
+    stop.disabled = true;
+    clearInterval(intervalId);
+    intervalId = null;
+})
+
+reset.addEventListener('click', function (e) {
+    reset.disabled = true;
+    timeLag = 0;
+    updateTime();
+})
